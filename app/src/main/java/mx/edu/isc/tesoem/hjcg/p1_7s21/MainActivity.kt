487fb4aa80b8c418bd9f18401757430245d7a51c
Hazael <haza.el78@hotmail.com>
2023-09-25 01:16:00 -0600

    cambios

---
+++ app/src/main/java/mx/edu/isc/tesoem/hjcg/p1_7s21/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.isc.tesoem.hjcg.p1_7s21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
